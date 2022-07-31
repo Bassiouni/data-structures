b1ff4b1d6dd6ec7f0c9f59cde2c6c7b1aa30847f
Muhammad Bassiouni <mohmah345@gmail.com>
2022-08-01 01:19:47 +0200

    Adding remove fn to singly linked kist

--- LinkedList/single.cc
+++ LinkedList/single.cc
@@ -5,6 +5,10 @@ struct Node
 {
   T data;
   Node<T> *next;
+
+  Node() = default;
+  Node(T &val) : data(val), next(nullptr) {}
+  Node(T &&val) : Node(val) {}
 };
 
 template <typename T>
@@ -15,8 +19,7 @@ public:
 
   void insert(T value)
   {
-    Node<T> *temp = new Node<T>;
-    temp->data = value;
+    Node<T> *temp = new Node<T>(value);
 
     if (!this->head)
     {
@@ -32,13 +35,42 @@ public:
   {
     Node<T> *temp = this->head;
 
+    std::cout << ((temp) ? "[" : "Empty!");
+
     while (temp)
     {
-      std::cout << temp->data << " -> ";
+      std::cout << temp->data << "]";
       temp = temp->next;
+      if (temp)
+        std::cout << " -> [";
     }
 
-    std::cout << "null" << std::endl;
+    std::cout << std::endl;
+  }
+
+  void remove(T val)
+  {
+    Node<T> *prevptr = this->head;
+    Node<T> *currptr = prevptr;
+
+    if (currptr->data == val)
+    {
+      prevptr->next = currptr->next;
+      delete currptr;
+      return;
+    }
+    currptr = currptr->next;
+    while (currptr)
+    {
+      if (currptr->data == val)
+      {
+        prevptr->next = currptr->next;
+        delete currptr;
+        break;
+      }
+      currptr = currptr->next;
+      prevptr = prevptr->next;
+    }
   }
 
   ~LinkedList()
@@ -47,7 +79,7 @@ public:
     while (this->head)
     {
       temp = this->head->next;
-      std::cout << "deleting " << this->head->data << std::endl;
+      std::cout << "deleting node " << this->head->data << std::endl;
       delete this->head;
       this->head = temp;
     }
@@ -66,5 +98,9 @@ int main()
   l.insert(3);
   l.insert(4);
 
+  l.remove(5); // Testing with non-existing values to check for Seg Faults.
+  l.printAll();
+
+  l.remove(1);
   l.printAll();
 }
